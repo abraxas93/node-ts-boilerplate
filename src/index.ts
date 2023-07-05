bac9a97687ca95cfab593cc771a34c8ab6c951bf
Savchuk Alexandr <savchyk.a@gmail.com>
2023-07-05 16:00:58 +0545

    chore: restructured app folder design

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import 'reflect-metadata';
 import {initLogger} from './logger';
-import app, {bootstrapDependencies} from './app';
+import {container as app} from 'tsyringe';
+import {EventEmitter} from 'events';
+import {createMongoClient} from './infrastructure/database/mongo/createMongoClient';
+import {MongoClient} from 'mongodb';
 
 const logger = initLogger(__filename);
+
+export async function bootstrapDependencies() {
+  const mongoClient = await createMongoClient();
+  const eventEmitter = new EventEmitter();
+
+  app.register<EventEmitter>('EventEmitter', {useValue: eventEmitter});
+  app.register<MongoClient>('MongoClient', {useValue: mongoClient});
+}
+
 async function main() {
-  logger.info('bootstrap dependencies');
+  logger.info('bootstrap app dependencies');
   await bootstrapDependencies();
 }
 
