330779c985ff80f8ee667297c91f156dbdad66ac
Savchuk Alexandr <savchyk.a@gmail.com>
2023-07-06 14:15:06 +0545

    feat: add UserController

--- src/application/services/UserService.ts
+++ src/application/services/UserService.ts
@@ -1,13 +1,24 @@
 import {injectable, inject} from 'tsyringe';
 import {ICreateUser, IGetUser, IUserSignIn} from '../use-cases';
+import {UseCaseResult} from '../../types';
+import {IUserModel} from '../../domain/entities';
 
 /*
  * Facade class to wrap use cases
  * Responsible for use cases orchestration and providing interface access to domain logic
  */
 
+export interface IUserService {
+  createUser(email: string, password: string): Promise<UseCaseResult<string>>;
+  findUserById(id: string): Promise<UseCaseResult<IUserModel>>;
+  signInUser(
+    email: string,
+    password: string
+  ): Promise<UseCaseResult<IUserModel>>;
+}
+
 @injectable()
-export class UserService {
+export class UserService implements IUserService {
   constructor(
     @inject('IGetUser')
     private readonly getUserUseCase: IGetUser,
--- src/domain/repositories/index.ts
+++ src/domain/repositories/index.ts
@@ -1,7 +1,1 @@
-import {User} from '../entities';
-
-export interface IUserRepository {
-  findById(id: string): Promise<User | null>;
-  findByEmail(email: string): Promise<User | null>;
-  insertOne(user: User): Promise<string>;
-}
+export * from './IUserRepository';
